35cd428d53b00fc3437940b5e3a829ce8b7bc6af
MacBookAir2019 <alonzo.choque@icloud.com>
2024-10-14 21:02:06 -0400

    MacBookAir2019 20241014 21:05 New First Project TODO LON

---
+++ 01_BasicCoreData/TodoLon/TodoLon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TodoLon
+//
+//  Created by MacBookAir2019 on 14/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+//#Preview {
+//    ContentView()
+//}
---
+++ 01_BasicCoreData/TodoLon/TodoLon/TodoLonApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoLonApp.swift
+//  TodoLon
+//
+//  Created by MacBookAir2019 on 14/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoLonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
